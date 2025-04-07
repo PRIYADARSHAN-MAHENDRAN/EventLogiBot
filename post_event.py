a105e26d2b90000b2090f50e2d37dd973266d99c
PRIYADARSHAN MAHENDRAN <priyadarshan09112004@gmail.com>
2025-04-07 09:51:37 +0530

    Update post_event.py

--- post_event.py
+++ post_event.py
@@ -197,7 +197,7 @@ for event_link in event_links_today:
                 {
                     "type": 2,  # Button
                     "style": 5,  # Link button
-                    "label": "View Event",
+                    "label": "View Event"
                     #"url": event_link
                     print(f"button1")
                 },
